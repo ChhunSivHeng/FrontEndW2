51b886ec36be689510fff421764ca03b83a70252
Chhun Sivheng <sivmiosu@gmail.com>
2025-02-06 14:27:21 +0700

    update

---
+++ Exercise 1/js/app.js
@@ -0,0 +1,4 @@
+let item3 = document.getElementById("container1").lastElementChild;
+let container2 = document.getElementById("container2");
+container2.appendChild(item3);
+console.log('container2');
